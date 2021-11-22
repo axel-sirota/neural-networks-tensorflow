26b6a17e9bad4735c1fba3fa69073876f13df064
Axel Sirota <axel.sirota@gmail.com>
2021-11-22 16:06:53 -0300

    test

--- classifier.py
+++ classifier.py
@@ -7,6 +7,15 @@ from tensorflow.keras.datasets import mnist
 # Task 1: Normalize the train and test images to be between 0 and 1. Expand the dimensions as well
 
 
+# Validation Task 1
+# This is for validation only, after you finish the task feel free to remove the prints and the exit command
+
+print(train_images.shape)
+print(np.max(train_images[0]))
+exit(0)
+
+# End of validation of task 1.
+
 # Task 2: Create a model that has the following structure:
 
 #   - 1 Conv2D layer of 10 3x3 filters
@@ -16,11 +25,10 @@ from tensorflow.keras.datasets import mnist
 
 model = None  # FILL ME
 
-
 print(model.summary())
 
-model.compile(optimizer = tf.keras.optimizers.Adam(),
-              loss = 'sparse_categorical_crossentropy',
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 # Task 3: Train the model for 2 epochs and pass the validation data to be the test images and labels
